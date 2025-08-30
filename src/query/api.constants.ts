6c471eb1ea00c0672950cfc616890c4467f28934
Veronica <benzaveronika@gmail.com>
2025-08-30 21:40:28 +0300

    fix(api): fix cors

--- src/query/api.constants.ts
+++ src/query/api.constants.ts
@@ -2,4 +2,5 @@ export const BASE_IMAGE_URL = 'https://training-api.clevertec.ru';
 export const getImagePath = (url: string | undefined) =>
     url ? `${BASE_IMAGE_URL}${url}` : undefined;
 
-export const BASE_URL = 'https://marathon-api.clevertec.ru';
+// export const BASE_URL = 'https://marathon-api.clevertec.ru';
+export const BASE_URL = 'https://cors-proxy.benzaveronika.workers.dev';
